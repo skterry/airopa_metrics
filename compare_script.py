2de46e68fb149e94c60773484586425ac2ba2c54
skterry <sterry4261@gmail.com>
2021-02-25 20:44:31 -0800

    star_name update

--- compare_script.py
+++ compare_script.py
@@ -16,20 +16,22 @@ Parameters:
 from airopa_test.compare_runs import plot_two_psfs,plot_residual_image,compare_fvu_on_star,compare_fvu_vs_mag
 
 #Plot two psfs params
-old_fit_dir = '../new-POOR-run/fit/'
-old_img_dir = '../new-POOR-run/image/'
-old_img_root = 'c0270'
+old_fit_dir = '../default/fit/'
+old_img_dir = '../default/image/'
+old_img_root = 'c0084'
 
-new_fit_dir = '../new-POOR-run/fit/'
-new_img_dir = '../new-POOR-run/image/'
-new_img_root = 'c0270'
+new_fit_dir = '../Smooth_1/fit/'
+new_img_dir = '../Smooth_1/image/'
+new_img_root = 'c0084'
 mode = 'variable'
 old_res = old_fit_dir + mode + '/' + old_img_root + '_res.fits'
 new_res = new_fit_dir + mode + '/' + new_img_root + '_res.fits'
-star_pos = [599, 111]
+
+star_name = 'irs29n'
+star_pos = [671, 764]
 
 #Choose which function to run:
 #plot_two_psfs(old_fit_dir,new_fit_dir,old_img_root,new_img_root,mode)
 #plot_residual_image(old_res,new_res,mode)
-compare_fvu_on_star(old_fit_dir, new_fit_dir, old_img_dir, new_img_dir, old_img_root,new_img_root, mode, star_pos)
+compare_fvu_on_star(old_fit_dir, new_fit_dir, old_img_dir, new_img_dir, old_img_root,new_img_root, mode, star_name, star_pos)
 #compare_fvu_vs_mag(old_fit_dir,new_fit_dir,old_img_dir,new_img_dir,old_img_root,new_img_root,mode)
